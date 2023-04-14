cf81beafff57a418a4fa844a42fb0117fa296c3e
BenseddikM <benseddikmohammed@gmail.com>
2023-04-14 22:33:34 +0200

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,57 @@
+import os
 import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
+from pathlib import Path
+from bs4 import BeautifulSoup
+import shutil
+
+# Your provided functions are placed here
+
+import os
+
+import streamlit as st
+import shutil
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+
+HOTJAR_TRACK_CODE = """
+<!-- Hotjar Tracking Code for https://benseddikm-streamlit-hotjar-app-drdwuu.streamlit.app/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3451826,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+"""
+
+def inject_hotjar():
+    HOTJAR_ELEMENT = "hotjar"
+    inject_script_to_streamlit(HOTJAR_TRACK_CODE, HOTJAR_ELEMENT)
+
+def inject_script_to_streamlit(script, element_id, inject_head=True):
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=element_id):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        if inject_head:
+            new_html = html.replace('<head>', '<head>\n' + script)
+        else:
+            new_html = html.replace('<body>', '<body>\n' + script)
+        index_path.write_text(new_html)
+        print(f'Injected {element_id}')
 
 # Generate sample data
 np.random.seed(42)
@@ -23,22 +73,8 @@ st.set_page_config(page_title='Fancy Streamlit App', layout='wide')
 # App title
 st.title('Fancy Streamlit App with Multiple Charts')
 
-# Add Hotjar tracking code
-hotjar_code = """
-<!-- Hotjar Tracking Code for my site -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3451826,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-"""
-
-st.markdown(hotjar_code, unsafe_allow_html=True)
+# Call the functions to inject the provided code
+inject_hotjar()
 
 # Sidebar
 st.sidebar.header('Options')
